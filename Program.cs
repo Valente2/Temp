8274f6f100e7bf8eafe1c00ec1995ee0c24a6e0b
Valente Vescio <valentevescio@gmail.com>
2026-03-16 19:09:50 -0400

    I fixed the test prog ;) <3

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 
+public class User
+{
+    public string? Name { get; set; }
+}
 class Program  
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
-        Console.GetLine("What is your name? ");
+        Console.WriteLine("What is your name? ");
+        User user = new User();
+        user.Name = Console.ReadLine();
+        Console.WriteLine($"Hello, {user.Name}");
     }
 }
